f02c0f9e43ae42ff0a4c91843b1707e2912c4b34
MaxKeable <max.keable488@gmail.com>
2023-04-21 10:34:26 +1000

    adding shape.js content to create 3 different shapes.

---
+++ Develop/utils/shapes.js
@@ -0,0 +1,30 @@
+class Shape{
+    // Defines a class called Shape which has a constructor intializing the color of the shape and sets the 'color' value.
+    
+        constructor(){
+            this.color=''
+        }
+        setColor(color){
+            this.color=(color);
+        }
+    }
+    // Creates Circle class that adds to Shape class and renders an SVG Circle with position, size, and fill color based on the color set.
+    class Circle extends Shape{
+        render(){
+            return `<circle cx="50%" cy="50%" r="100" height="100%" width="100%" fill="${this.color}">`
+        }
+    }
+    // Creates a Square class that adds to 'Shape' and renders an SVG Square it's fill color based on the current color set in 'color'. 
+    class Square extends Shape{
+        render(){
+            return `<rect x="50" height="200" width="200" fill="${this.color}">`
+        }
+    }
+    // Creates a Triangle class that extends Shape and renders an SVG using position, size, and fill color. 
+    class Triangle extends Shape{
+        render(){
+            return `<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.color}">`
+        }
+    };
+    
+    module.exports = {Circle, Square, Triangle}
