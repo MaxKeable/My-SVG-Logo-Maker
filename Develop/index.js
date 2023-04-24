61064d42b04584d808cb57dfd96220982df27b86
MaxKeable <max.keable488@gmail.com>
2023-04-24 10:24:48 +1000

    added logic to index.js and got an svg output off of prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,71 +1,118 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
-const generatemarkdown = require('./utils/generateMarkdown');
+const { Circle, Square, Triangle } = require('./utils/shapes');
 
-// TODO: Create an array of questions for user input
+class Svg {
+  constructor() {
+    this.textElement = '';
+    this.shapeElement = '';
+  }
+
+  render() {
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+  }
+
+  setTextElement(text, color) {
+    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
+
+  setShapeElement(shape) {
+    this.shapeElement = shape.render();
+  }
+}
+
+// creates questions using inquirer
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please describe the purpose and function of your project.',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please describe the installation process for your project.',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please describe the usage of your project.',
-    },
-    { 
-        type: 'checkbox',
-        name: 'license',
-        message: 'Select a license for your project.',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-    },
-    {
-        type: 'input',
-        name: 'contributions',
-        message: 'Please describe the contribution guidelines for your project.',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Please describe the test instructions for your project.',
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email address.',
-    },
+  {
+    type: 'input',
+    name: 'text',
+    message: 'TEXT: Enter up to (3) Characters:',
+  },
+  {
+    type: 'input',
+    name: 'text-color',
+    message: 'TEXT COLOR: Enter a color keyword (OR a hexadecimal number):',
+  },
+  {
+    type: 'input',
+    name: 'shape-color',
+    message: 'SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):',
+  },
+  {
+    type: 'list',
+    name: 'pixel-image',
+    message: 'Choose which Pixel Image you would like?',
+    choices: ['Circle', 'Square', 'Triangle'],
+  },
 ];
 
-// TODO: Create a function to write README file
+// Function write data to file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  console.log(`Writing [${data}] to file [${fileName}]`);
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Congratulations, you have Generated a logo.svg!');
+  });
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then((responses) => {
-        console.log('Generating README file ...');
-        writeToFile('README.md', generatemarkdown({... responses}));
-    });
-}
+async function init() {
+  console.log('Starting init');
+  let svgString = '';
+  const svg_file = 'logo.svg';
 
-// Function call to initialize app
-init();
+  // Prompt the user for answers to questions
+  const answers = await inquirer.prompt(questions);
+
+  // User text
+  const userText = answers.text.slice(0, 3);
+  console.log(`User text: [${userText}]`);
+
+  // User font color
+  const userFontColor = answers['text-color'];
+  console.log(`User font color: [${userFontColor}]`);
+
+  // User shape color
+  const userShapeColor = answers['shape-color'];
+  console.log(`User shape color: [${userShapeColor}]`);
+
+  // User shape type
+  const userShapeType = answers['pixel-image'];
+  console.log(`User entered shape = [${userShapeType}]`);
+
+  // User shape
+  let userShape;
+  if (userShapeType.toLowerCase() === 'square') {
+    userShape = new Square();
+    console.log('User selected Square shape');
+  } else if (userShapeType.toLowerCase() === 'circle') {
+    userShape = new Circle();
+    console.log('User selected Circle shape');
+  } else if (userShapeType.toLowerCase() === 'triangle') {
+    userShape = new Triangle();
+    console.log('User selected Triangle shape');
+  } else {
+    console.log('Invalid shape!');
+    return;
+  }
+  userShape.setColor(userShapeColor);
+
+  // Create a new Svg instance and add the shape and text elements to it
+  const svg = new Svg();
+  svg.setTextElement(userText, userFontColor);
+  svg.setShapeElement(userShape);
+  svgString = svg.render();
+
+  // Print shape to log
+console.log("Displaying shape:\n\n" + svgString);
+//document.getElementById("svg_image").innerHTML = svgString;
+
+console.log("Shape generation complete!");
+console.log("Writing shape to file...");
+writeToFile(svg_file, svgString); 
+}
+init()
+
+
+
